7a4ae70f45af8fc27147a2609c0fa0c9875d8413
lochen1031 <matt051031@gmail.com>
2025-06-03 23:01:45 +0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
